bf2987bd8c5197b57e396e9d9fe3c88ac3088b0d
Garbhan Kenneally <68166450+GarbhanK@users.noreply.github.com>
2021-09-24 01:21:31 +0100

    added -q flag to download call
    makes the terminal output generally cleaner, removes unnecessary info.

--- ytdl.sh
+++ ytdl.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # where do you want to save your videos?
-
 folder="/home/garbhank/Videos/ytdl_vids" 
 
 printf "Garbhan's Youtube Download Script 💾 \n"
-echo "Paste the link..."
+echo "Gimme the link..."
 read link
 youtube-dl -i --get-filename --skip-download $link
-echo "Would you like to download these videos? [Y/N]"
+echo "These the ones you want? [Y/N]"
 read ans 
 if [[ "$ans" == "Y" ]]; then
     cd $folder
-    youtube-dl -f 18 -v $link
+    youtube-dl -q -f 18 -v $link
 else
     echo "Bye bye :)"
 fi
