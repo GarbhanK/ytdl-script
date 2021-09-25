20cb4b52633af48549fd44f834b8d02bdfc70501
Garbhan Kenneally <68166450+GarbhanK@users.noreply.github.com>
2021-09-25 16:24:35 +0100

    Added support for downloading audio

--- ytdl.sh
+++ ytdl.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 # where do you want to save your videos?
-folder="/home/garbhank/Videos/ytdl_vids" 
+videofolder="/home/garbhank/Videos/ytdl_vids"
+audiofolder="/home/garbhank/Music/ytdl_music"
+aardvark="/home/garbhank/Downloads/aardvark.txt"
 
 printf "Garbhan's Youtube Download Script 💾 \n"
 printf "\n"
 echo "Gimme the link..."
 read link
 printf "\n"
-youtube-dl -i --get-filename --skip-download $link
+youtube-dl -i --get-title --skip-download $link
 printf "\n"
 echo "These the ones you want? [Y/N]"
 read ans 
-if [[ "$ans" == "Y" ]]; then
-    cd $folder
-    youtube-dl -q -f 18 -v $link
-else
-    echo "Bye bye :)"
+if [[ $ans == "Y" ]]; then
+    echo "Just the audio? [Y/N]"
+    read isAudio
+    if [[ $isAudio == "Y" ]]; then
+        cd $audiofolder
+        cat $aardvark
+        printf "\n"
+        youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --embed>
+    elif [[ "$isAudio" == "N" ]]; then
+        cd $videofolder
+        cat $aardvark
+        printf "\n"
+        youtube-dl -f 18 -v -o '%(title)s.%(ext)s' --rm-cache-dir $link
+    else
+        echo "Bye bye :)"
+    fi
 fi
 
 printf "\n"
